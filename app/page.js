68ea629acf1d53964869bc414da4351f1bd1e5e5
Radwan <radwan.basket@gmail.com>
2023-12-24 20:25:26 +0300

    EBLA3

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started NOW by editing&nbsp;
+          Get started NOW WLAK by editing&nbsp;
           <code className={styles.code}>app/page.js</code>
         </p>
         <div>
