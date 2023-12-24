66ba13a2fc0125a5a2c9ba2daa1b0a1623fd0250
Radwan <radwan.basket@gmail.com>
2023-12-24 20:44:37 +0300

    CHANGING PAGE.JS AGAIN

--- app/page.js
+++ app/page.js
@@ -2,5 +2,5 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <main className={styles.main}>ABOMAJED NUMBER ONE</main>;
+  return <main className={styles.main}>ABO AL-NOOR NUMBER ONE</main>;
 }
